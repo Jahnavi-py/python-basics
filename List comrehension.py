1320714858cd5cc4296ff231345a7919d7ee5fa1
Jahnavi-py <vallabhanenijahnavi4@gmail.com>
2024-12-18 23:11:42 -0500

    My learnings and practice

--- List comrehension.py
+++ List comrehension.py
@@ -1,5 +1,3 @@
 #Use a list comprehension to create a list of squares from 1 to 10
-#numbers = [1, 2, 3, 4, 5,6,7,8,9,10]
-#squared_list= [num**2 for num in numbers]
 squared_list= [num**2 for num in range(1,11)]
 print(f"list comprehension to create a list of squares from 1 to 10 is {squared_list}")
