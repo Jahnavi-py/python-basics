fc70a7413182670b10e2bff049e6b31e2f48c884
Jahnavi-py <vallabhanenijahnavi4@gmail.com>
2024-12-31 10:16:57 -0500

    My learnings and practice

--- String manipulation - anagrams.py
+++ String manipulation - anagrams.py
@@ -3,7 +3,6 @@ string1 =  "listen"
 string2 = "silent"
 string1 = string1.replace(" ", "").lower()
 string2 = string2.replace(" ", "").lower()
-#are_anagrams = (sorted(string1) == sorted(string2))
 if sorted(string1) == sorted(string2) :
     print(f"'{string1} 'and '{string2}' are anagrams.")
 else:
