a07a16a65109ea88af7834c9fd341dbd5605b7f3
Jahnavi-py <vallabhanenijahnavi4@gmail.com>
2024-12-31 10:14:31 -0500

    My learnings and practice

--- String manipulation - Alphabetic character.py
+++ String manipulation - Alphabetic character.py
@@ -1,5 +1,5 @@
 #Extract only the alphabetic characters from a string.
 string = "!!!@Python is a programming language, which is easy to read and write.!!!@"
-print(f"The string is : {string}")
+print(f"The string : {string}")
 alphabetic_char = "".join(char for char in string if char.isalpha())
-print(f"The string '{string}' Aplhabetic characters is : '{alphabetic_char}'")
+print(f"Aplhabetic characters : '{alphabetic_char}'")
