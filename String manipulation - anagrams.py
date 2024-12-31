5f68ba64b768c68280b2f7db23a5abe45617c812
Jahnavi-py <vallabhanenijahnavi4@gmail.com>
2024-12-31 10:16:31 -0500

    My learnings and practice

--- String manipulation - anagrams.py
+++ String manipulation - anagrams.py
@@ -8,3 +8,11 @@ if sorted(string1) == sorted(string2) :
     print(f"'{string1} 'and '{string2}' are anagrams.")
 else:
     print(f"'{string1}' and '{string2}' are not anagrams.")
+string3 = "Pen"
+string4 = "Python"
+string3 = string3.replace(" ", "").lower()
+string4 = string4.replace(" ", "").lower()
+if sorted(string3) == sorted(string4) :
+    print(f"'{string3} 'and '{string4}' are anagrams.")
+else:
+    print(f"'{string3}' and '{string4}' are not anagrams.")
