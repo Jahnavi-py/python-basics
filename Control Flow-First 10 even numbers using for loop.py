ab7c3c78d047a7cd74e05296679cba1dca9b0b0f
Jahnavi-py <vallabhanenijahnavi4@gmail.com>
2025-01-30 11:12:33 -0500

    My learnings and practice

--- Control Flow-First 10 even numbers using for loop.py
+++ Control Flow-First 10 even numbers using for loop.py
@@ -1,8 +1,10 @@
 # Write a program to print the first 10 even numbers using a loop.
-number = int(input("Enter the number :"))
+'''number = int(input("Enter the number :"))
 for i in range(10):
     if number % 2 == 0:
         print(f"The first 10 even numbers are : {number}")
         number += 2
     else:
-        print("The given number is not even")
+        print("The given number is not even")'''
+for i in range(1, 11):
+    print("The first 10 even numbers are : ", i*2)
