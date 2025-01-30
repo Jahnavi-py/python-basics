10aa32c053e3899b162a16c833f2480ed6f92fc7
Jahnavi-py <vallabhanenijahnavi4@gmail.com>
2025-01-30 15:43:35 -0500

    My learnings and practice

---
+++ Dict store student.py
@@ -0,0 +1,8 @@
+#Create a dictionary to store student names and their grades
+students_grades = {
+    "Alice": 90,
+    "Bob": 85,
+    "Charlie": 92,
+    "Diana": 88
+}
+print(f"The students name and grades are : {students_grades}")
