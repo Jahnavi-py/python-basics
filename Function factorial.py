1b9ac08f3bd0392d1298c2d6032005ebb0d00327
Jahnavi-py <vallabhanenijahnavi4@gmail.com>
2025-01-30 11:22:08 -0500

    My learnings and practice

--- Function factorial.py
+++ Function factorial.py
@@ -1,8 +1,13 @@
 #Write a function to calculate the factorial of a number.
-def factorial(num):
+"""def factorial(num):
     result = 1
     for i in range(1, num + 1):
         result *= i
-    return result
+    return result"""
+def factorial(n):
+    if n == 0 or n == 1:
+        return 1
+    else:
+        return n * factorial(n - 1)
 number = int(input("Enter a number to find its factorial: "))
 print(f"The factorial of {number} is: {factorial(number)}")
