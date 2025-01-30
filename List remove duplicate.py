cda57b2eb2cad2cd7bcbfcbbb8b3a93a7131e5db
Jahnavi-py <vallabhanenijahnavi4@gmail.com>
2025-01-30 15:37:00 -0500

    My learnings and practice

--- List remove duplicate.py
+++ List remove duplicate.py
@@ -1,5 +1,5 @@
 #Remove duplicate elements from a list.
 my_list = [1, 2, 3, 2, 5]
-#my_list.remove(2)
+print(f"The list is : {my_list}")
 unique_list = list(set(my_list))
 print(f"List after removing duplicates: {unique_list}")
