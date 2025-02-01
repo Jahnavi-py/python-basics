49c2119e0f3aa8e7e178af9be6c318f06acd398c
Jahnavi-py <vallabhanenijahnavi4@gmail.com>
2025-02-01 09:47:12 -0500

    My learnings and practice

--- ExceptionHandling - Nestedtry.py
+++ ExceptionHandling - Nestedtry.py
@@ -5,12 +5,12 @@ def handle_exception():
     try:
         print("Trying to access an element in the list:")
         try:
-            print(my_list[5])  # This will raise an IndexError
+            print(my_list[5])
         except IndexError:
             print("Error: Index out of range in the list!")
         print("\nTrying to access a key in the dictionary:")
         try:
-            print(my_dict['address'])  # This will raise a KeyError
+            print(my_dict['address'])
         except KeyError:
             print("Error: Key not found in the dictionary!")
     except Exception as e:
