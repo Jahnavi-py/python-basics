74966d96521ae6a7ced6132eafebf844f6b1c1ee
Jahnavi-py <vallabhanenijahnavi4@gmail.com>
2025-02-01 01:00:48 -0500

    My learnings and practice

--- Function plindrome.py
+++ Function plindrome.py
@@ -1,6 +1,6 @@
 #Create a function to check if a string is a palindrome.
-def is_palindrome(string):
-    string = string.replace(" ", "").lower()
+def is_palindrome(String):
+    string = String.replace(" ", "").lower()
     return string == string[::-1]
 input_string = input("Enter a string: ")
 if is_palindrome(input_string):
